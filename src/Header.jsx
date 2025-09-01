10847e55d817b8888fc807a7eb9bf91621803f48
v.yukhnevych <v.yukhnevych@bfirst.tech>
2025-09-01 20:23:07 +0200

    path for icon changed

--- src/Header.jsx
+++ src/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
   return (
     <header className="fixed max-w-screen w-full h-17 xl:h-19 2xl:h-25 px-4 xl:px-20 2xl:px-[162px] lg:px-40 top-0 bg-gr10 flex items-center justify-between text-lg outline outline-gr15 z-100 default-text">
       <Link to="/" className="flex-shrink-0 h-7 xl:h-[34px] 2xl:h-12 ">
-        <img src="/icons/logo.svg" alt='Logo' className='h-full'/>
+        <img src="icons/logo.svg" alt='Logo' className='h-full'/>
       </Link>
       <nav className="absolute left-1/2 -translate-x-1/2 flex items-center 2xl:gap-[30px] xl:gap-6" aria-label='test'>
         {links.map(({ href, label }) => (
